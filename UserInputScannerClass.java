f8a438b143aaa20f6e9e73462382fb35459604b0
Prince Bansal <prince.bansal008@gmail.com>
2022-08-10 13:26:33 +0530

    basics added

---
+++ UserInputScannerClass.java
@@ -0,0 +1,27 @@
+class UserInputScannerClass{
+    public static void main(String []args){
+        // To Take input from user
+
+        // nextByte(), nextShort(), nextInt(), nextLong()
+        // nextFloat(), nextDouble()
+        // nextBoolean()
+        // next() => take input string without space like scanf() in c
+        // nextLine() => take input string with space like gets() in c
+        // nextChar() --> Does not exist
+
+        // They Belongs to Scanner class and they are Non static => they'll get called using an Object
+        // Why scanner class ? because in Java we can take input from internet, sockets (IP + Port number),
+        // keyboard, files etc
+
+        // let there is an Emp class in java
+        // then how to create it's object ?
+
+        // Emp E = new Emp();
+        // Where:
+        // E = reference variable
+        // new is operator
+        // Emp() => paranthesis is compulsory, if parameters then pass in it
+
+
+    }
+}
