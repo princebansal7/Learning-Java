33a6cd7a978da706d7d84c0262746ff039d60e4d
Prince Bansal <prince.bansal008@gmail.com>
2022-11-24 19:28:54 +0530

    Folder updated & Assignments started

---
+++ Assignment-1/Program2.java
@@ -0,0 +1,10 @@
+public class Program2 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+
+//        or
+
+        System.out.println("Hello\nWorld");
+    }
+}
---
+++ Assignment-1/Program3.java
@@ -0,0 +1,5 @@
+public class Program3 {
+    public static void main(String[] args) {
+        System.out.println("\"Teacher's Day\"");
+    }
+}
