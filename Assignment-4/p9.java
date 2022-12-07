0080e5b8d9f456cb21bad60faa35a38e724cd874
Prince Bansal <prince.bansal008@gmail.com>
2022-12-08 01:34:26 +0530

    logic updated

--- Assignment-4/p9.java
+++ Assignment-4/p9.java
@@ -1,13 +1,19 @@
 
 public class p9 {
     public static void main(String[] args) {
-        int n = 1554;
-        int count = 0;
-        while (n > 0) {
-            count++;
-            n /= 10;
-        }
-        if (count == 3)
+        int n = 154;
+
+        // int count = 0;
+        // while (n > 0) {
+        //     count++;
+        //     n /= 10;
+        // }
+        // if (count == 3)
+        //     System.out.println("yes 3 digit number");
+        // else
+        //     System.out.println("No");
+
+        if (n > 99 && n < 1000)
             System.out.println("yes 3 digit number");
         else
             System.out.println("No");
